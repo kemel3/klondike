3b78d3c3a04dca5a9fec364cb698294169529f40
Kemel3 <kemelZX@gmail.com>
2019-11-08 09:50:07 +0100

    Added full screen

--- src/main/java/com/codecool/klondike/Klondike.java
+++ src/main/java/com/codecool/klondike/Klondike.java
@@ -15,10 +15,10 @@ public class Klondike extends Application {
         Card.loadCardImages();
         Game game = new Game(primaryStage);
         game.setTableBackground(new Image("/table/green.png"));
-
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
+        primaryStage.setMaximized(true);
     }
 
     public static void main(String[] args) {
@@ -29,6 +29,4 @@ public class Klondike extends Application {
     public void start(Stage primaryStage) {
         Klondike.initStage(primaryStage);
     }
-
-
 }
